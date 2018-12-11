578759baaee318fb3e6cf07a970d5f98e21f51ee
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2018-12-11 18:43:53 +0100

    day-11: Second answer

--- 2018/11.py
+++ 2018/11.py
@@ -3,13 +3,18 @@
 file = open("input/11.txt", "r")
 
 serialNumber = 0
-fuelGrid = [[0] * 300 for i in range(300)]
-squares = [[0] * 297 for i in range(297)]
+squaresArray = []
 
 def parseInput():
     global serialNumber
     serialNumber = int(file.readline())
 
+def initSquaresArray():
+    global squaresArray
+    for size in range(300):
+        squaresArray.append([[0] * (300 - size) for i in range(300 - size)])
+        print(size)
+        print(len(squaresArray[-1]))
 
 def calcPowerLevel(x, y):
     global serialNumber
@@ -27,45 +32,60 @@ def calcPowerLevel(x, y):
     return powerLevel
 
 
-def generateFuelGrid():
-    global fuelGrid
-    for i in range(300):
-        for j in range(300):
-            fuelGrid[i][j] = calcPowerLevel(i+1, j+1) # x and y start at 1
-
-def calcSquarePowerLevel(x, y):
-    global fuelGrid
+def calcSquarePowerLevel(x, y, size):
+    global globalSquaresArray
     squarePowerLevel = 0
-    for i in range(3):
-        for j in range(3):
-            squarePowerLevel = squarePowerLevel + fuelGrid[x + i][y +j]
+
+    if size == 1: # Generate fuel grid
+        squarePowerLevel = calcPowerLevel(x + 1, y + 1) # x and y start at 1
+    else:
+        fuelPower = squaresArray[0]
+        squarePowerLevel = squaresArray[size - 2][x][y]
+        for i in range(size):
+            squarePowerLevel = squarePowerLevel + fuelPower[x + i][y + size - 1]
+        for j in range(size - 1):
+            squarePowerLevel = squarePowerLevel + fuelPower[x + size - 1][y + j]
+
     return squarePowerLevel
 
+
 def generateSquares():
-    global squares
-    for i in range(297):
-        for j in range(297):
-            squares[i][j] = calcSquarePowerLevel(i, j)
+    global squaresArray
+
+    for size, squares in enumerate(squaresArray):
+        print(size)
+        for i in range(300 - size):
+            for j in range(300 - size):
+                squares[i][j] = calcSquarePowerLevel(i, j, size + 1)
+
+
+
 
 def findMaxSquare():
-    global squares
+    global squaresArray
     maxVal = 0
     maxX = 0
     maxY = 0
-    for i in range(297):
-        for j in range(297):
-            if squares[i][j] > maxVal:
-                maxVal = squares[i][j]
-                maxX = i + 1
-                maxY = j + 1
-    return maxX, maxY
+    maxSize = 0
+    for size, squares in enumerate(squaresArray):
+        for i in range(300 - size):
+            for j in range(300 - size):
+                if squares[i][j] > maxVal:
+                    maxVal = squares[i][j]
+                    maxX = i + 1
+                    maxY = j + 1
+                    maxSize = size + 1
+    return maxX, maxY, maxSize
+
+
 
 def main():
     parseInput()
-    generateFuelGrid()
+    initSquaresArray()
     generateSquares()
-    x, y = findMaxSquare()
-    print("X %d, Y %d" % (x, y))
+    x, y, size = findMaxSquare()
+
+    print("X %d Y %d size %d" %(x, y, size))
 
 
 if __name__ == '__main__':
