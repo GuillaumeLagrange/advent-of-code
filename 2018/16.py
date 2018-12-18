ff24d0ff47842558b3bb01c02fbb22dd9e4f49c4
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2018-12-18 14:53:26 +0100

    day16: First answer

--- 2018/16.py
+++ 2018/16.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 
-file = open("input/XXX.txt", "r")
+file = open("input/16.txt", "r")
+
+sequence = []
+instructions = []
 
 def addr(reg, arg):
     ret = reg.copy()
@@ -133,12 +136,50 @@ def eqrr(reg, arg):
 
 
 def parseInput():
-    for line in file:
-        print(line)
+    global instructions
+    global sequence
+    instructions = [
+        addr, addi,
+        mulr, muli,
+        banr, bani,
+        borr, bori,
+        setr, seti,
+        gtir, gtri, gtrr,
+        eqir, eqri, eqrr,
+    ]
+
+    while True:
+        line = file.readline().strip()
+        if "Before" not in line:
+            break
+        before = eval("".join(line.split(" ")[1::]))
+        line = file.readline().strip()
+        args = eval("[" + ", ".join(line.split(" ")) + "]")
+        line = file.readline().strip()
+        after = eval("".join(line.split(" ")[1::]))
+        file.readline()
+
+        sequence.append((before, args, after))
 
 
 def main():
+    global instructions
+    global sequence
+    instructionsNb = []
     parseInput()
+    for i, element in enumerate(sequence):
+        nb = 0
+        for f in instructions:
+            if f(element[0], element[1][1::]) == element[2]:
+                nb += 1
+        instructionsNb.append(nb)
+    a = 0
+    for n in instructionsNb:
+        if n >=3:
+            a += 1
+    print(a)
+
+
 
 
 if __name__ == '__main__':
